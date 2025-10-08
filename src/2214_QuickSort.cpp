fe511dc0e386f95826c024bca26f1b2d88555917
Kyunana097 <Kyunana097@gmail.com>
2025-10-08 21:45:20 +0800

    thi

--- src/2214_QuickSort.cpp
+++ src/2214_QuickSort.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <algorithm>
 
+//分类主函数
+void Quick_Sort() {
+
+}
+
+//被调用的递归函数
+void quicksort(int a[], int x) {
+
+}
+
+int main() {
+    int x;
+    while (!(std::cin >> x))
+        
+}
 //
 // Created by 62705 on 2025/10/7.
 //
